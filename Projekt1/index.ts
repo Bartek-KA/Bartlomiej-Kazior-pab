0d2b220d41db4740a4749b50f727fefd3dcc1d25
iambartek <bartlomiej.kazior@microsoft.wsei.edu.pl>
2022-03-09 18:29:07 +0100

    Step 10

--- Projekt1/index.ts
+++ Projekt1/index.ts
@@ -1,6 +1,31 @@
 const express = require('express')  
 const app = express()  
 app.get('/', function (req, res) {  
-  res.send('Hello World')  
+  const num1 = +req.query.num1
+  const num2 = +req.query.num2
+  const operation = req.query.operation;
+  switch(operation){
+    case "add" : 
+    const add = num1 + num2
+    res.send(add.toString())
+    break;
+    case "subtract" : 
+    const subtract = num1 - num2 
+    res.send(subtract.toString())
+    break;
+    case "multiply" : 
+    const multiply = num1 * num2
+    res.send(multiply.toString())
+    break;
+    case "divide" :
+    const divide = num1 / num2
+    res.send(divide.toString())
+    break;
+
+    default:
+      alert('Brak operacji')
+  }
+
+
 })  
 app.listen(3000)  
